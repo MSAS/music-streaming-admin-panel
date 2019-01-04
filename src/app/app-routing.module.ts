2a19f50080522f97bd79dd4b83f5d2dd17030e49
saurabh pandey <saurabh.pandey@mindfulsas.com>
2019-01-04 09:35:10 +0530

    Routing Issue FIxed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,6 @@ const routes: Routes = [
     path: 'drive', children: [
       { path: '', component: DocumentsComponent },
       { path: ':id', component: FoldersComponent }],
-      canActivate: [RoleGuard], pathMatch: 'full' 
   },
 
 
